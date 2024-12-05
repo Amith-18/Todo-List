adf05baf21cf8071e5f1405a236da6cec70433f3
Amith-18 <amithsanil@outlook.com>
2024-12-05 14:42:03 +0400

    Adding file

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+const App = () => {
+
+  let x ='Amith';
+  let array =['user1','user2','user3','user4']
+  return (
+    <div>
+      {x}
+      {array.map((user)=>{
+        return <h2>{user}</h2>
+      })}
+    </div>
+  )
+ 
+}
+export default App
